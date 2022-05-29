96a960d8e53a2e2166f0fa7c35b9f314059c066b
Aviatorscode2 <victoryakubu5@gmail.com>
2022-05-29 02:16:05 +0100

    layout for the about page

---
+++ src/pages/About.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+
+function About() {
+  return (
+    <>
+      <h1 className="text-6xl mb-4">Github Finder</h1>
+      <p className="mb-4 text-2xl font-light">
+        A React app to search GitHub profiles and see profile details.
+      </p>
+      <p className="text-lg text-gray-400">
+        Version <span className="text-black">1.0.0</span>
+      </p>
+      <p className="text-lg text-gray-400">
+        By:
+        <a className="text-black" href="https://twitter.com/edoboi_">
+          theAviatorCodes2
+        </a>
+      </p>
+    </>
+  );
+}
+
+export default About;
