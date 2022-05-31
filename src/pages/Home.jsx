d6d71e9420e7bc10835faacadf37e310d627ec3f
Aviatorscode2 <victoryakubu5@gmail.com>
2022-05-31 02:26:40 +0100

    Created the user search component

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,12 @@
 import UserResults from '../components/users/UserResults';
+import UserSearch from '../components/users/UserSearch';
 
 function Home() {
   return (
-    <div>
+    <>
+      <UserSearch />
       <UserResults />
-    </div>
+    </>
   );
 }
 
