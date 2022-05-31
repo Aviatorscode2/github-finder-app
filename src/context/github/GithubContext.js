f8c1aeaa1baad617c90db44b348667bd6056c0d4
Aviatorscode2 <victoryakubu5@gmail.com>
2022-05-31 01:58:01 +0100

    Cleaning up fetch users

--- src/context/github/GithubContext.js
+++ src/context/github/GithubContext.js
@@ -9,12 +9,14 @@ const GITHUB_TOKEN = process.env.REACT_APP_GITHUB_TOKEN;
 export const GithubProvider = ({ children }) => {
   const initialState = {
     users: [],
-    loading: true,
+    loading: false,
   };
 
   const [state, dispatch] = useReducer(githubReducer, initialState);
 
+  //   this function was to get initial users for testing purposes
   const fetchUsers = async () => {
+    setLoading();
     const response = await fetch(`${GITHUB_URL}/users`, {
       headers: {
         Authorization: `token ${GITHUB_TOKEN}`,
@@ -28,6 +30,12 @@ export const GithubProvider = ({ children }) => {
       payload: data,
     });
   };
+
+  //   Set loading state function
+  const setLoading = () =>
+    dispatch({
+      type: 'SET_LOADING',
+    });
   return (
     <GithubContext.Provider
       value={{
